5844e259984c0ecdbff9bcf625370befdbdcce03
matiasbellaud <bellaud.matias@gmail.com>
2024-01-16 16:23:39 +0100

    :sparkles: | add login request user

--- agence_superhero/app/Http/Controllers/API/UserController.php
+++ agence_superhero/app/Http/Controllers/API/UserController.php
@@ -9,7 +9,7 @@ use App\Models\User;
 class UserController extends Controller
 {
 
-    public function store(Request $request)
+    public function register(Request $request)
     {
         try {
             $request->validate([
@@ -28,8 +28,33 @@ class UserController extends Controller
         $user->firstName = $request->input('firstName');
         $user->lastName = $request->input('lastName');
         $user->email = $request->input('email');
-        $user->password = $request->input('password');
+        $user->password = hash('sha256',$request->input('password'));
         $user->save();
         return response()->json(['succes' => 'true'], 200);
+    }
+
+    public function login(Request $request)
+    {
+        try {
+            $request->validate([
+                'email' => ['required', 'string', 'max:100'],
+                'password' => ['required', 'string', 'max:50'],
+            ]);
+        } catch (ValidationException $e) {
+            return response()->json([
+                'succes' => 'false',
+                'errors' => $e->errors(),
+            ], 422);
+        }
+       
+        $user = User::where('email', $request->email)->first();
+        if ($user==null){
+            return response()->json(-1);
+        }
+        $logPassword = hash('sha256',$request->password);
+        if ($logPassword == $user->password){
+            return response()->json($user->id);
+        }
+        return response()->json(-1);
     }
 }
--- agence_superhero/routes/api.php
+++ agence_superhero/routes/api.php
@@ -4,9 +4,13 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\API\HeroController;
+use App\Http\Controllers\API\UserController;
 
 // Route::apiresource('heros', HeroController::class);
 Route::post('/post/getHeroesByUser', [HeroController::class, 'getHeroesByUser']);
 Route::post('/post/createHero', [HeroController::class, 'store']);
+
+Route::post('/post/register', [UserController::class, 'register']);
+Route::post('/post/login', [UserController::class, 'login']);
 
 // Route::get('/vehicles/showName/{t}', [VehicleController::class, 'showName']);
